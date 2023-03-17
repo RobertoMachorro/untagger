e8eb0d6927f2abe701c3dbdf3b3f24831e53a0ff
Roberto Machorro <7190436+RobertoMachorro@users.noreply.github.com>
2023-03-17 17:54:29 -0400

    Implemented parsing, search and output. This does CSV type of output.

--- untagger/main.swift
+++ untagger/main.swift
@@ -6,6 +6,32 @@
 //
 
 import Foundation
+import SwiftSoup
 
-print("Hello, World!")
+guard CommandLine.arguments.count == 3 else {
+	fatalError("Missing input file and folder/sub-menu name.")
+}
 
+let filePath = CommandLine.arguments[1]
+let seekFolderName = CommandLine.arguments[2]
+
+do {
+	let html = try String(contentsOfFile: filePath, encoding: .utf8)
+
+	let doc: Document = try SwiftSoup.parse(html)
+	let folders = try doc.select("dt")
+
+	print("\"Title\",\"Hyperlink\"")
+	for folder in folders {
+		let name = try folder.select("h3").first()?.html()
+		if let name: String = name, name == seekFolderName {
+			for link in try folder.select("a") {
+				let caption = try link.text()
+				let href = try link.attr("href")
+				print("\"\(caption)\",\"\(href)\"")
+			}
+		}
+	}
+} catch {
+	print(error)
+}
